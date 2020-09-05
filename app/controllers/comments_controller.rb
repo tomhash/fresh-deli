a272d0bc680fab51840dd780a00deab1661f25b4
tomhash <tomonarihashikami@gmail.com>
2020-09-05 16:03:27 +0900

    コメント機能の実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  def create
+    @item = Item.find(params[:item_id])
+    comment = Comment.new(comment_params)
+    if comment.save
+      redirect_to item_path(@item)
+    else
+      flash.now[:alert] = 'コメントを入力してください。'
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
+
