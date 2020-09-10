2d6ec16938c4223cc75ef8ff816c5bc2cbdd50c6
tomhash <tomonarihashikami@gmail.com>
2020-09-10 14:40:07 +0900

    チャット機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'messages/new'
   devise_for :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   resources :users, only: [:show,:edit,:update]
+  get 'messages', to: 'messages#new'
+  resources :messages, only: [:create]
   resources :items do
     resources :comments, only: :create 
+    resources :transactions, only:[:create,:index]
   end
+  mount ActionCable.server => '/cable'
 end
